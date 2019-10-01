bd4a814e5e6929234f703d673ae992ddc7c1e8cf
calvitox <saudadista@gmail.com>
2019-10-01 17:00:43 -0500

    sidebar toggle functionality

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ElementRef } from '@angular/core';
 
 declare const $: any;
 declare interface RouteInfo {
@@ -19,12 +19,46 @@ export const ROUTES: RouteInfo[] = [
 })
 export class SidebarComponent implements OnInit {
   menuItems: any[];
+  private sidebar: any;
+  private sidebar_wrapper: any;
+  private mainpanel: any;
+  public sidebarVisible: boolean;
 
-  constructor() { }
+  constructor(private element: ElementRef) { }
 
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
+    this.sidebar  = document.getElementsByClassName('sidebar')[0];
+    this.sidebar_wrapper = document.getElementsByClassName('sidebar-wrapper')[0];
+    this.mainpanel = document.getElementsByClassName('main-panel')[0];
+    this.sidebarVisible = true;
   }
+
+  sidebarOpen() {
+    this.sidebar.classList.remove("sb-closed");
+    this.sidebar_wrapper.classList.remove("sb-closed");
+    this.mainpanel.classList.remove('sb-closed');
+    this.sidebarVisible = true;
+  };
+
+  sidebarClose() {
+      const body = document.getElementsByTagName('body')[0];
+      this.sidebar.classList.add("sb-closed");
+      this.sidebar_wrapper.classList.add("sb-closed");       
+      this.mainpanel.classList.add('sb-closed');
+      this.sidebarVisible = false;
+  };
+
+  sidebarToggle() {
+      // const toggleButton = this.toggleButton;
+      // const body = document.getElementsByTagName('body')[0];
+      if (this.sidebarVisible === false) {
+          this.sidebarOpen();
+      } else {
+          this.sidebarClose();
+      }
+  };
+
   isMobileMenu() {
       if ($(window).width() > 991) {
           return false;
