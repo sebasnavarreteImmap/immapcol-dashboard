78ee51cb08f74e33d103774c43c5faadae2d8c1c
calvitox <saudadista@gmail.com>
2020-04-14 14:15:59 -0500

    sources section modified

--- src/app/immap-sources/immap-sources.component.ts
+++ src/app/immap-sources/immap-sources.component.ts
@@ -1,53 +1,123 @@
-import {Component, ViewChild} from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
+import {Component, PipeTransform} from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { FormControl, NgControl } from '@angular/forms';
 
-import {MatPaginator} from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators'
 
-const ELEMENT_DATA = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+interface Source {
+  id?: number;
+  name: string;
+  flag: string;
+  area: number;
+  population: number;
+}
+
+const SOURCES: Source[] = [
+  {
+    name: 'Russia',
+    flag: 'f/f3/Flag_of_Russia.svg',
+    area: 17075200,
+    population: 146989754
+  },
+  {
+    name: 'France',
+    flag: 'c/c3/Flag_of_France.svg',
+    area: 640679,
+    population: 64979548
+  },
+  {
+    name: 'Germany',
+    flag: 'b/ba/Flag_of_Germany.svg',
+    area: 357114,
+    population: 82114224
+  },
+  {
+    name: 'Portugal',
+    flag: '5/5c/Flag_of_Portugal.svg',
+    area: 92090,
+    population: 10329506
+  },
+  {
+    name: 'Canada',
+    flag: 'c/cf/Flag_of_Canada.svg',
+    area: 9976140,
+    population: 36624199
+  },
+  {
+    name: 'Vietnam',
+    flag: '2/21/Flag_of_Vietnam.svg',
+    area: 331212,
+    population: 95540800
+  },
+  {
+    name: 'Brazil',
+    flag: '0/05/Flag_of_Brazil.svg',
+    area: 8515767,
+    population: 209288278
+  },
+  {
+    name: 'Mexico',
+    flag: 'f/fc/Flag_of_Mexico.svg',
+    area: 1964375,
+    population: 129163276
+  },
+  {
+    name: 'United States',
+    flag: 'a/a4/Flag_of_the_United_States.svg',
+    area: 9629091,
+    population: 324459463
+  },
+  {
+    name: 'India',
+    flag: '4/41/Flag_of_India.svg',
+    area: 3287263,
+    population: 1324171354
+  },
+  {
+    name: 'Indonesia',
+    flag: '9/9f/Flag_of_Indonesia.svg',
+    area: 1910931,
+    population: 263991379
+  },
+  {
+    name: 'Tuvalu',
+    flag: '3/38/Flag_of_Tuvalu.svg',
+    area: 26,
+    population: 11097
+  },
+  {
+    name: 'China',
+    flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
+    area: 9596960,
+    population: 1409517397
+  }
 ];
+
+function search(text: string, pipe: PipeTransform): Source[] {     
+
+  return SOURCES
+      .filter(source => {
+        const term = text.toLowerCase();
+        return source.name.toLowerCase().includes(term)
+            || pipe.transform(source.area).includes(term)
+            || pipe.transform(source.population).includes(term);
+      });
+}
 
 @Component({
   selector: 'app-immap-sources',
   templateUrl: './immap-sources.component.html',
-  styleUrls: ['./immap-sources.component.scss']
+  styleUrls: ['./immap-sources.component.scss'],
+  providers: [DecimalPipe]
 })
 
 export class ImmapSourcesComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  sources$: Observable<Source[]>;
+  filter = new FormControl('');
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
-  
-  ngOnInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  constructor(pipe: DecimalPipe) {
+    this.sources$ = this.filter.valueChanges.pipe(startWith(''),map(text => search(text, pipe)));
   }
 }
 
